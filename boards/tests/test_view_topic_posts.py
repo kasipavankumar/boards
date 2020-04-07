30e824de1311de3ae32e486e60c4a7ab4d495933
Code-Plus-Coffee <m416a@protonmail.com>
2020-04-07 22:15:34 +0530

    Fixed a test.

--- boards/tests/test_view_topic_posts.py
+++ boards/tests/test_view_topic_posts.py
@@ -37,4 +37,4 @@ class TopicPostsTests(TestCase):
 
     def test_view_function(self):
         view = resolve('/boards/1/topics/1/')
-        self.assertEquals(view.func.class_view, PostListView)
+        self.assertEquals(view.func.view_class, PostListView)
