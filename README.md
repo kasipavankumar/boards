78bd0c31fc1022ccdb71dce0f7c93bf22060cf0e
Code-Plus-Coffee <m416a@protonmail.com>
2020-04-07 12:04:03 +0530

    Add Readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Discuss Forum
+
+An online discussion forum developed using Django.
+
+Thanks to [SimpleIsBetterThanComplex](https://simpleisbetterthancomplex.com/series/beginners-guide/1.11/) for the tutorial on which this project is based on.
+
+`Code Plus Coffee`
--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config, Csv
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,13 +22,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ay*mpv2fua!t#&f8y*zxrp1!a82ux8_)zvvn3(#bpo9!#=rpg)'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', case=Csv())
 
 # Application definition
 
@@ -80,10 +81,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
 
 
